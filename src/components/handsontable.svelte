5a14b93bfbd69fc5eb017228694fa61832b13504
erichebbard-Oxygen8 <ericisdeveloper@gmail.com>
2023-06-07 21:43:37 -0700

    Works!
    This allows us to sort and move around column sizes

---
+++ src/components/handsontable.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+	<script src="node_modules/handsontable/dist/handsontable.full.min.js"
+					on:load={() => isScriptLoaded = true} />
+	<link href="node_modules/handsontable/dist/handsontable.full.min.css" rel="stylesheet" media="screen" 
+				on:load={() => isStyleLoaded = true} />
+</svelte:head>
+
+<script>
+	export let data;
+    export let columns;
+
+	let isScriptLoaded = false;
+	let isStyleLoaded = false;
+	let isPageReady = false;
+	$: isPageReady = isScriptLoaded && isStyleLoaded;
+
+	function gridInit(node) {
+		new Handsontable(node, {
+            data:data,
+            columns:columns,
+            width: '100%',
+            height: 'auto',
+            colHeaders: true,
+            rowHeaders: true,
+            colWidths: 100,
+            columnSorting: true,
+            manualColumnResize: true,
+			licenseKey: "non-commercial-and-evaluation"
+		});
+	}
+</script>
+
+{#if isPageReady}
+	<div use:gridInit></div>
+{/if}
