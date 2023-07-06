b7216001793bafe6cd3e40e863181557a5e4a746
erichebbard-Oxygen8 <ericisdeveloper@gmail.com>
2023-07-05 21:42:38 -0700

    Adding Tailwind and SkeletonUI

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-    kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+
+	preprocess: [vitePreprocess({})]
 };
